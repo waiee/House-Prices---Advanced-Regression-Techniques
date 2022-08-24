62953a83fc00683730480d96a95c7d2bdcb9cea9
waiee <waiee_z@yahoo.com>
2022-08-24 22:17:34 +0800

    file uploader

--- code/main.py
+++ code/main.py
@@ -2,8 +2,13 @@ import streamlit as st
 import plotly_express as px
 import pandas as pd
 
+
+
 #App Title
 st.title("Data Visualization App")
 
 #Add a sidebar
 st.sidebar.subheader("Visualization Settings")
+
+#Setup file upload
+st.sidebar.file_uploader(label="Upload your CSV or Excel file.")
