42fce3f7445344047b380c472e9e21c1682e4243
waiee <waiee_z@yahoo.com>
2022-08-24 22:21:12 +0800

    configuration sidebar

--- code/main.py
+++ code/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import plotly_express as px
 import pandas as pd
 
-
+#Configuration
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 #App Title
 st.title("Data Visualization App")
